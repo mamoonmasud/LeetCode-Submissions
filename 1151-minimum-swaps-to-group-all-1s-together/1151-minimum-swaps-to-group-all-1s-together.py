160cc6d9386df0267f428fe9f6001225b13cc424
Mamoon Masud <40490102+mamoonmasud@users.noreply.github.com>
2022-01-19 21:37:05 -0600

    Time: 918 ms (42.63%), Space: 17.6 MB (35.36%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -1,21 +1,46 @@
 class Solution:
 
     def minSwaps(self, data: List[int]) -> int:
+
+#         ones = sum(data)
+#         if ones <= 1:
+#             return 0
+#         minswaps = ones
+#         w = deque(maxlen = ones)
+        
+#         curr = 0
+        
+#         for b in data:
+#             if len(w) == ones:
+#                 curr -= w[0]
+                
+#             w.append(b)
+#             curr += b
+#             if len(w) == ones:
+#                 minswaps = min(minswaps, ones - curr)
+                
+#         return min(minswaps, ones - curr)
+    
         ones = sum(data)
-        if ones <= 1:
+        if ones <2:
             return 0
-        minswaps = ones
-        w = deque(maxlen = ones)
+        start = 0
+        end = ones-1
         
-        curr = 0
+        zeros = data[start:ones].count(0)
         
-        for b in data:
-            if len(w) == ones:
-                curr -= w[0]
+        minswap = zeros
+        while end < len(data) -1:
+            prev = data[start]
+            start +=1
+            end +=1
+            
+            cur = data[end]
+            if cur ==1 and prev == 0:
+                zeros -=1
                 
-            w.append(b)
-            curr += b
-            if len(w) == ones:
-                minswaps = min(minswaps, ones - curr)
-                
-        return min(minswaps, ones - curr)
+            elif cur ==0 and prev ==1:
+                zeros+=1
+            minswap = min(minswap, zeros)
+            
+        return minswap
