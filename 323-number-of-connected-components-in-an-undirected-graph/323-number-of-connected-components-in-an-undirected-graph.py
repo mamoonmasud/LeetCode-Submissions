0a441e62b03a69b34a8dd864afa4a67ad919d0d0
Mamoon Masud <40490102+mamoonmasud@users.noreply.github.com>
2022-01-23 20:14:27 -0600

    Time: 177 ms (21.41%), Space: 15.5 MB (75.53%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,30 +1,59 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        visit = set()
+#         visit = set()
         
-        adj = {i: [] for i in range(n)}
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
+#         adj = {i: [] for i in range(n)}
+#         for n1, n2 in edges:
+#             adj[n1].append(n2)
+#             adj[n2].append(n1)
             
-        def dfs(node, prev):
-            if node in visit:
-                return 
-            visit.add(node)
-            for neigh in adj[node]:
-                if neigh == prev:
-                    continue
-                else:
-                    dfs(neigh, node)
+#         def dfs(node, prev):
+#             if node in visit:
+#                 return 
+#             visit.add(node)
+#             for neigh in adj[node]:
+#                 if neigh == prev:
+#                     continue
+#                 else:
+#                     dfs(neigh, node)
                     
-            return
-        connected_comp = 0
-        for i in range(n):
-            if i not in visit:
-                dfs(i, -1)
-                connected_comp+=1 
+#             return
+#         connected_comp = 0
+#         for i in range(n):
+#             if i not in visit:
+#                 dfs(i, -1)
+#                 connected_comp+=1 
                 
-        return connected_comp
+#         return connected_comp
             
                 
+        parent = [i for i in range(n)]
+        rank = [1]*n
         
+        def find(n1):
+            res = n1
+            while (res != parent[res]):
+                parent[res] = parent[parent[res]] 
+                res = parent[res]
+                
+            return res
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            
+            if p1 == p2:
+                return 0
+            
+            if rank[p2] > rank[p1]:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            return 1
+        res = n
+        
+        for n1, n2 in edges:
+            res -= union(n1, n2)
+            
+        return res
