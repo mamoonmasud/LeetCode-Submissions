d24d13aec06cdeac4fbc0cb49ca656ac4153521c
Mamoon Masud <40490102+mamoonmasud@users.noreply.github.com>
2022-01-25 19:56:31 -0600

    Time: 436 ms (45.31%), Space: 19 MB (18.66%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
@@ -1,35 +1,55 @@
 class Solution:
     def uniqueLetterString(self, s: str) -> int:
         
-        if s is None:
-            return 
+        memo = defaultdict(list)
+        for idx, char in enumerate(s):
+            memo[char].append(idx)
+            
+        result = 0
         
-        lastSeen = {}
-        count_sub = 0
+        for key, val in memo.items():
+            for i, idx in enumerate(val):
+                if i-1 >=0:
+                    left = idx - val[i-1]
+                else:
+                    left = idx - (-1)
+                    
+                if i+1 < len(val):
+                    right = val[i+1] - idx
+                else:
+                    right = len(s) - idx
+                
+                result += left*right
+        return result
+#         if s is None:
+#             return 
         
-        last_s_cnt = 0
+#         lastSeen = {}
+#         count_sub = 0
         
-        for i in range(len(s)):
-            last_two_idx = lastSeen.get(s[i], None)
+#         last_s_cnt = 0
+        
+#         for i in range(len(s)):
+#             last_two_idx = lastSeen.get(s[i], None)
             
-            if not last_two_idx:
-                crnt_s_cnt = last_s_cnt + i + 1
+#             if not last_two_idx:
+#                 crnt_s_cnt = last_s_cnt + i + 1
                 
-                lastSeen[s[i]] = (-1, i)
+#                 lastSeen[s[i]] = (-1, i)
                 
-            else:
-                snd_lst_s_idx , last_s_idx = last_two_idx
+#             else:
+#                 snd_lst_s_idx , last_s_idx = last_two_idx
                 
-                num_of_suffix_without = i - 1 - last_s_idx
-                num_of_suffix_with_one = last_s_idx -  snd_lst_s_idx
+#                 num_of_suffix_without = i - 1 - last_s_idx
+#                 num_of_suffix_with_one = last_s_idx -  snd_lst_s_idx
                 
-                crnt_s_cnt = last_s_cnt +  1 + num_of_suffix_without - num_of_suffix_with_one
+#                 crnt_s_cnt = last_s_cnt +  1 + num_of_suffix_without - num_of_suffix_with_one
                 
-                lastSeen[s[i]] = (last_s_idx, i)
+#                 lastSeen[s[i]] = (last_s_idx, i)
             
             
             
-            count_sub += crnt_s_cnt
-            last_s_cnt = crnt_s_cnt
+#             count_sub += crnt_s_cnt
+#             last_s_cnt = crnt_s_cnt
             
-        return count_sub
+#         return count_sub
