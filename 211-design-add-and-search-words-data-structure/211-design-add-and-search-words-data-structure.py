82608ddcdc90da541aac8929d81dc78fc21fe4d5
Mamoon Masud <40490102+mamoonmasud@users.noreply.github.com>
2022-01-28 07:54:06 -0600

    Time: 320 ms (73.82%), Space: 28.8 MB (59.11%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -1,44 +1,37 @@
-
-# class WordDictionary:
-
-
-# Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
-
-class TrieNode:
+class Trie:
     def __init__(self):
-        self.children = {}
+        self.children= {}
         self.word = False
-
+        
+        
 class WordDictionary:
-
     def __init__(self):
-        self.root  = TrieNode()
-               
-    def addWord(self, word: str) -> None:
+        self.root = Trie()
+        
+    def addWord(self, word):
         curr = self.root
         
-        for charac in word:
-            if charac not in curr.children:
-                curr.children[charac] = TrieNode()
-            curr= curr.children[charac]
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Trie()
+                
+            curr= curr.children[c]
             
         curr.word = True
         
-    def search(self, word: str) -> bool:
         
-        def dfs(j, root):
-            curr = root
+    def search(self, word: str) ->bool:
+        def dfs(j, node):
+            curr = node
             for i in range(j, len(word)):
                 c = word[i]
-                if c ==".":
+                
+                if c == ".":
                     for child in curr.children.values():
                         if dfs(i+1, child):
                             return True
+                        
                     return False
-                    
                 else:
                     if c not in curr.children:
                         return False
@@ -46,4 +39,7 @@ class WordDictionary:
                     
             return curr.word
         return dfs(0, self.root)
-                    
+    
+        
+        
+            
