3514dc38366ca86f1806bb35fef5126554940d1b
Mamoon Masud <mamoonmasud@MacBookPro.lan>
2025-02-09 19:51:12 +0500

    Lowest Common Ancestor of a Binary Search Tree - Time: 64 ms, Space: 20 mb

---
+++ 235-lowest_common_ancestor_of_a_binary_search_tree/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                return root
